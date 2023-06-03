233a8e7c1d81f5aee90f599127bd8009db8dda26
nzone56 <jpereirap@unal.edu.co>
2023-06-02 21:51:43 -0500

    Observer with event_handler

--- src/path_observer.py
+++ src/path_observer.py
@@ -19,19 +19,39 @@ class PathObserver(FileSystemEventHandler):
         if event.is_directory:
             return None
         
+         # Log the modification
+        print(f"Modified: {event.src_path}")
         
 
     def on_deleted(self, event) -> None:
         '''
         Method that is called when a file is deleted.
         '''
+        print(f"Deleted: {event.src_path}")
+        
         pass
 
 
 
-def start_observer(path:str) -> None:
+def start_observer(path):
     '''
     This function is responsible for starting the observer.
     '''
     observer = Observer()
     event_handler = PathObserver(path)
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    
+    try:
+        while True:
+            # Keep the observer running
+            pass
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
+
+# Example usage
+if __name__ == "__main__":
+    path_to_watch = "/path/to/watch"
+    start_observer(path_to_watch)
